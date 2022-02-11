856b6379b0d40644173b24eb20729c0ba54ea8b1
delorme38 <83075378+delorme38@users.noreply.github.com>
2022-02-11 16:34:31 -0500

    mise a jour sw

--- sw.js
+++ sw.js
@@ -1,56 +1,51 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installation - v1');
-  e.waitUntil(
-    caches.open('v1').then((cache) => {
+this.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
       return cache.addAll([
-        'index.html',
-        'index.js',
-        'Carte.js',
-        'favicon.ico',
-        'bootstrap-5.1.3-dist/css/bootstrap.min.css',
-        'icons-1.7.2/font/bootstrap-icons.css',
-        'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-        'img/img-1.jpg',
-        'img/img-2.jpg',
-        'img/img-3.jpg',
-        'img/img-4.jpg',
-        'img/img-5.jpg',
-        'img/img-6.jpg',
-        'img/img-7.jpg',
-        'img/img-8.jpg',
-        'img/img-9.jpg',
-        'img/icon-96x96.png',
-        'img/icon-192x192.png',
-        'img/icon-256x256.png',
-        'img/icon-384x384.png',
-        'img/icon-512x512.png'
+        '/index.html',
+        '/index.js',
+        '/Carte.js',
+        '/favicon.ico',
+        '/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+        '/icons-1.7.2/font/bootstrap-icons.css',
+        '/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+        '/img/img-1.jpg',
+        '/img/img-2.jpg',
+        '/img/img-3.jpg',
+        '/img/img-4.jpg',
+        '/img/img-5.jpg',
+        '/img/img-6.jpg',
+        '/img/img-7.jpg',
+        '/img/img-8.jpg',
+        '/img/img-9.jpg',
+        '/img/icon-96x96.png',
+        '/img/icon-192x192.png',
+        '/img/icon-256x256.png',
+        '/img/icon-384x384.png',
+        '/img/icon-512x512.png'
       ]);
     })
   );
 });
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener('fetch', function (event) {
   console.log("Fetching ..." + event.request.url);
   event.respondWith(cacheOrNetwork(event.request).catch(() => fallbackVersPageHorsLigne()));
 });
 
 
 function cacheOrNetwork(request) {
-return fromCache(request).catch(() => fetch(request));
+  return fromCache(request).catch(() => fetch(request));
 };
 
 function fromCache(request) {
-return caches.open('v1').then(function (cache) {
-  return cache.match(request).then(function (matching) {
-    return matching || Promise.reject('no-match');
+  return caches.open('v1').then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
   });
-});
 }
 
 function fallbackVersPageHorsLigne() {
-return caches.match("/page-hors-ligne.html");
+  return caches.match("/page-hors-ligne.html");
 }
-
-self.addEventListener('activate', (e) => {
-  console.log('active');
-});
